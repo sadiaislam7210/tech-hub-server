250eaaf6e07ddac1d3ea8b6cda05cdd3b3fade8d
Sadia Islam <sadia.islam7210@gmail.com>
2022-05-21 18:38:07 +0600

    mongodb connection done

--- index.js
+++ index.js
@@ -21,6 +21,94 @@ const client = new MongoClient(uri, {
 });
 
 
+// for JWT 
+const accessToken = async (req, res, next) =>{
+
+}
+const run = async () => {`1 QaZ`
+  try {
+    await client.connect();
+    const productsCollection = client.db("Electra").collection("products");
+
+    // load all item from database
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const allProduct = await cursor.toArray();
+      res.send(allProduct);
+    });
+
+    // load single item using _id
+    app.get("/singleProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
+
+    // load multiple item using user email 
+    app.get("/my-items/:searchEmail", async (req, res)=>{
+        const query = { email: req.params.searchEmail };
+        const cursor = productsCollection.find(query);
+        const findedProductsBasedOnEmail = await cursor.toArray();
+        res.send(findedProductsBasedOnEmail);
+    })
+
+    // add single item to database
+    app.post("/add", async (req, res) => {
+      const newItem = req.body;
+      console.log(newItem);
+      res.send({ result: "data received!" });
+      const result = await productsCollection.insertOne(newItem);
+      console.log("User Inserted. ID: ", result.insertedId);
+    });
+
+    // update a product
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      console.log(updatedProduct);
+      const updatedDoc = {
+        $set: {
+            name: updatedProduct.name ,
+            email: updatedProduct.email,
+            productName: updatedProduct.productName,
+            imgLink: updatedProduct.imgLink,
+            quantity: updatedProduct.quantity,
+            pricePerItem: updatedProduct.pricePerItem,
+            supplierName: updatedProduct.supplierName,
+            description: updatedProduct.description,    
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // delete a product from database 
+    app.delete(`/deleteProduct/:id`, async (req, res) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        console.log(query);
+        const result = await productsCollection.deleteOne(query);
+        res.send(result);
+    })
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
+   
+/********************************************\
+            MongoDB Connection End
+\********************************************/
+
+
 
 // Create root API
 app.get("/", (req, res) => {
