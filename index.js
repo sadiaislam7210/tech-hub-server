bc6c52edcce7f82c81973d165a40b3d4d065e08e
Sadia Islam <sadia.islam7210@gmail.com>
2022-05-21 18:17:25 +0600

    for middleware

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// For Middleware
+const app = express();
+app.use(cors());
+app.use(express.json());
